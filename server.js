2551f73bbf0107b214e6c5ae4faec91939b65230
Chris.Franklin <cfrank306@gmail.com>
2021-11-09 14:30:49 -0600

    updated controllers

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+require('dotenv').config();
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
