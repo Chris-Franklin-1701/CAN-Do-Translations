d692d1b727193f1bc143f6799149bd59f0cfc389
Alex Cwik <alexcwik1490@gmail.com>
2021-11-10 11:47:05 -0600

    added signup form to login.handlebars

--- models/index.js
+++ models/index.js
@@ -1,49 +1,10 @@
 const User = require('./user');
 const Comments = require('./comments');
 
-<<<<<<< HEAD
-User.hasMany(Text, {
-    foreignKey: 'user_id',
-});
-=======
 User.hasMany(Comments);
->>>>>>> 657d0abaf4f6bf31258bf5f05c96ba991154c97e
 
 Comments.belongsTo(User);
 
-<<<<<<< HEAD
-Comments.hasMany(Translations);
-
-<<<<<<< HEAD
-Text.hasOne(Language, {
-    foreignKey: 'lang_id'
-});
-
-Translation.hasOne(Text, {
-    foreignKey: 'text_id',
-})
-
-Translation.hasMany(Language, {
-    foreignKey: 'lang_id'
-});
-
-Language.hasMany(Translation, {
-    foreignKey: 'trans_id'
-});
-
-Rando.hasMany(Translation, {
-    foreignKey: 'trans_id'
-});
-
-Rando.hasMany(Language, {
-    foreignKey: 'lang_id'
-}); 
-=======
-Translations.belongsTo(Comments);
->>>>>>> 657d0abaf4f6bf31258bf5f05c96ba991154c97e
-
-=======
->>>>>>> bd718da4fc4654b80ad05038e50537a712ca8921
 module.exports = {
     User,
     Comments,
