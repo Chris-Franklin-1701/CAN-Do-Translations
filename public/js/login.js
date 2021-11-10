0e19dc247e606b66420c89e9f98e7cec6cb29a25
Alex Cwik <alexcwik1490@gmail.com>
2021-11-10 14:13:37 -0600

    fixed the signup page to load on a different page than the login

--- public/js/login.js
+++ public/js/login.js
@@ -22,29 +22,4 @@ const loginFormHandler = async (event) => {
   }
 };
 
-/* async function signupFormHandler (event){
-  event.PreventDefault();
-}
-
-const username = document.querySelector('#username').value.trim();
-const email = document.querySelector('#email').value.trim();
-const password = document.querySelector('#password').value.trim();
-
-if (username && email && password) {
-  const response = await fetch('/api/users/', {
-  method: 'POST',
-  body: JSON.stringify({ username, email, password}),
-  headers: { 'Content-Type': 'application/json' }
-  });
-
-  if (response.ok) {
-      console.log('Sign Up Successful!');
-      document.location.replace('/dashboard/signup');
-  } else {
-      alert(response.statusText);
-  }
-}
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler); */
-
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
