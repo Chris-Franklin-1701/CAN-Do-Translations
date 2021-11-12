469b3127a754cdb2235ee7ac58bdbf264ad4652a
Nelson Rivera <riveranelson99@gmail.com>
2021-11-12 09:50:28 -0600

    Added a 2nd translation functionality

--- public/js/fullAccess.js
+++ public/js/fullAccess.js
@@ -1,45 +1,45 @@
-// console.log('basic access js loaded.')
-// window.onload = function () {
-//     console.log('window loaded. onload running')
-//     const input = document.querySelector("#input");
-//     const btnTest = document.querySelector("#btn-test");
-//     const output = document.querySelector("#output");
-//     console.log(JSON.stringify(btnTest))
-//     // create a function that calls on basicAccessRoutes
-//     // function to fetch the POST route, with the client input.
-//     //print output to the html file as #output var.
+console.log('basic access js loaded.')
+window.onload = function () {
+    console.log('window loaded. onload running')
+    const input = document.querySelector("#input");
+    const btnTest = document.querySelector("#btn-test");
+    const output = document.querySelector("#output");
+    console.log(JSON.stringify(btnTest))
+    // create a function that calls on basicAccessRoutes
+    // function to fetch the POST route, with the client input.
+    //print output to the html file as #output var.
 
 
-//     // function constructUrl(text) {
-//     //     return serverUrl + "?text=" + text;
-//     // }
+    // function constructUrl(text) {
+    //     return serverUrl + "?text=" + text;
+    // }
 
-//     // function errorHandler(error) {
-//     //     alert("API call limit reached. Please try again in hour.");
-//     // }
+    // function errorHandler(error) {
+    //     alert("API call limit reached. Please try again in hour.");
+    // }
 
-//     function fullTranslation(event) {
-//         event.preventDefault();
-//         console.log('button pressed')
-//         const inputText = input.value;
-//         fetch('/api/comments', {
-//             method: 'POST',
-//             body: JSON.stringify({ input: inputText }),
-//             headers: { "Content-Type": "application/json" }
-//         })
-//             .then(request => request.json())
-//             .then(json => {
-//                 const translatedText = json;
-//                 output.innerText = translatedText;
-//                 console.log(json);
-//             })
-//             .catch(errorHandler)
-//     }
-//     function errorHandler() {
-//         alert('nooooooooooo')
-//     }
+    function fullTranslation(event) {
+        event.preventDefault();
+        console.log('button pressed')
+        const inputText = input.value;
+        fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ input: inputText }),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(request => request.json())
+            .then(json => {
+                const translatedText = json.output;
+                output.innerText = translatedText;
+                console.log(json);
+            })
+            .catch(errorHandler)
+    }
+    function errorHandler() {
+        alert('nooooooooooo')
+    }
 
 
-//     btnTest.addEventListener("click", fullTranslation);
+    btnTest.addEventListener("click", fullTranslation);
 
-// }
+}
